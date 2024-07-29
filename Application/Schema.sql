e8af6a6120b4b754ef5454e3084010b2452e71bd
wwobert <110064776+wobertt@users.noreply.github.com>
2024-07-28 23:15:57 -0400

    copy schema from yulong/schema

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -8,34 +8,92 @@ CREATE TABLE users (
 CREATE TABLE portfolios (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     user_id UUID NOT NULL,
-    name TEXT NOT NULL,
-    UNIQUE(user_id, name)
+    portfolio_name TEXT NOT NULL,
+    UNIQUE(user_id, portfolio_name)
 );
-CREATE TABLE holdings (
-    portfolio UUID NOT NULL,
+CREATE TABLE portfolio_holds (
+    portfolio_id UUID NOT NULL,
     symbol TEXT NOT NULL,
     amount INT NOT NULL,
-    PRIMARY KEY(portfolio, symbol)
+    PRIMARY KEY(symbol, portfolio_id)
 );
 CREATE TABLE stocklists (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL UNIQUE,
     user_id UUID NOT NULL,
-    name TEXT NOT NULL,
+    list_name TEXT NOT NULL,
     is_public BOOLEAN DEFAULT false NOT NULL,
     category TEXT NOT NULL,
-    PRIMARY KEY(user_id, name)
+    UNIQUE(user_id, list_name)
 );
 CREATE INDEX stocklists_user_id_index ON stocklists (user_id);
 CREATE TABLE transactions (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    user_from UUID DEFAULT NULL,
-    user_to UUID DEFAULT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    symbol TEXT NOT NULL,
-    amount TEXT NOT NULL
+    amount INT NOT NULL,
+    portfolio_from UUID NOT NULL,
+    portfolio_to UUID NOT NULL,
+    symbol TEXT NOT NULL
 );
 CREATE INDEX transactions_created_at_index ON transactions (created_at);
-ALTER TABLE holdings ADD CONSTRAINT holdings_ref_portfolio FOREIGN KEY (portfolio) REFERENCES portfolios (id) ON DELETE NO ACTION;
+CREATE TYPE status AS ENUM ('ACCEPTED', 'REJECTED', 'PENDING');
+CREATE TABLE friends (
+    user_from UUID NOT NULL,
+    user_to UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    status status,
+    PRIMARY KEY(user_from, user_to)
+);
+CREATE INDEX friends_user_from_index ON friends (user_from);
+CREATE INDEX friends_user_to_index ON friends (user_to);
+CREATE TABLE accesses (
+    user_id UUID NOT NULL,
+    owner_id UUID NOT NULL,
+    list_id UUID NOT NULL,
+    PRIMARY KEY(list_id, owner_id, user_id)
+);
+CREATE INDEX accesses_user_id_index ON accesses (user_id);
+CREATE TABLE reviews (
+    user_id UUID NOT NULL,
+    list_id UUID NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY(user_id, list_id)
+);
+CREATE INDEX reviews_user_id_index ON reviews (user_id);
+CREATE INDEX reviews_list_id_index ON reviews (list_id);
+CREATE TABLE list_contains (
+    list_id UUID NOT NULL,
+    symbol TEXT NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY(list_id, symbol)
+);
+CREATE TABLE stocks (
+    symbol TEXT PRIMARY KEY NOT NULL
+);
+CREATE TABLE history (
+    "timestamp" DATE NOT NULL,
+    open REAL,
+    high REAL,
+    low REAL,
+    "close" REAL NOT NULL,
+    volume INT NOT NULL,
+    symbol TEXT NOT NULL,
+    PRIMARY KEY(symbol, timestamp)
+);
+CREATE INDEX transactions_portfolio_from_index ON transactions (portfolio_from);
+CREATE INDEX transactions_portfolio_to_index ON transactions (portfolio_to);
+ALTER TABLE accesses ADD CONSTRAINT accesses_ref_list_id FOREIGN KEY (list_id) REFERENCES stocklists (id) ON DELETE NO ACTION;
+ALTER TABLE accesses ADD CONSTRAINT accesses_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
+ALTER TABLE friends ADD CONSTRAINT friends_ref_user_from FOREIGN KEY (user_from) REFERENCES users (id) ON DELETE NO ACTION;
+ALTER TABLE friends ADD CONSTRAINT friends_ref_user_to FOREIGN KEY (user_to) REFERENCES users (id) ON DELETE NO ACTION;
+ALTER TABLE history ADD CONSTRAINT history_ref_symbol FOREIGN KEY (symbol) REFERENCES stocks ON DELETE NO ACTION;
+ALTER TABLE list_contains ADD CONSTRAINT list_contains_ref_list_id FOREIGN KEY (list_id) REFERENCES stocklists (id) ON DELETE NO ACTION;
+ALTER TABLE list_contains ADD CONSTRAINT list_contains_ref_symbol FOREIGN KEY (symbol) REFERENCES stocks ON DELETE NO ACTION;
+ALTER TABLE portfolio_holds ADD CONSTRAINT portfolio_holds_ref_portfolio_id FOREIGN KEY (portfolio_id) REFERENCES portfolios (id) ON DELETE NO ACTION;
+ALTER TABLE portfolio_holds ADD CONSTRAINT portfolio_holds_ref_symbol FOREIGN KEY (symbol) REFERENCES stocks ON DELETE NO ACTION;
 ALTER TABLE portfolios ADD CONSTRAINT portfolios_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
+ALTER TABLE reviews ADD CONSTRAINT reviews_ref_list_id FOREIGN KEY (list_id) REFERENCES stocklists (id) ON DELETE NO ACTION;
+ALTER TABLE reviews ADD CONSTRAINT reviews_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
 ALTER TABLE stocklists ADD CONSTRAINT stocklists_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
-ALTER TABLE transactions ADD CONSTRAINT transactions_ref_user_from FOREIGN KEY (user_from) REFERENCES users (id) ON DELETE NO ACTION;
-ALTER TABLE transactions ADD CONSTRAINT transactions_ref_user_to FOREIGN KEY (user_to) REFERENCES users (id) ON DELETE NO ACTION;
+ALTER TABLE transactions ADD CONSTRAINT transactions_ref_portfolio_from FOREIGN KEY (portfolio_from) REFERENCES portfolios (id) ON DELETE NO ACTION;
+ALTER TABLE transactions ADD CONSTRAINT transactions_ref_portfolio_to FOREIGN KEY (portfolio_to) REFERENCES portfolios (id) ON DELETE NO ACTION;
+ALTER TABLE transactions ADD CONSTRAINT transactions_ref_symbol FOREIGN KEY (symbol) REFERENCES stocks ON DELETE NO ACTION;
